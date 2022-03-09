d1a1431d81473b8af9853b5f22d576fab6cb41b3
MatejPapac <papac.tmrip@gmail.com>
2022-03-09 11:52:09 +0100

    Update konfiguracije

--- app/konfiguracija.php
+++ app/konfiguracija.php
@@ -2,18 +2,27 @@
 
 if($_SERVER['SERVER_ADDR']==='127.0.0.1'){
     $url='http://zavrsni.xyz/';
+    $dev=true;
+    $baza=[
+        'server'=>'localhost',
+        'baza'=>'zavrsni',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ];
 }else{
     $url='http://www.zr-papac.shop/';
+    $dev=false;
+    $baza=[
+        'server'=>'zr-papac.shop',
+        'baza'=>'zavrsni',
+        'korisnik'=>'Papac',
+        'lozinka'=>'$Rak95xpt'
+    ];
 }
 
 return [
- 'dev'=>true,
-'url'=>'http://zavrsni.xyz/',
-'naslovApp'=>'Bistro Loora',
-'baza'=>[
-    'server'=>'localhost',
-    'baza'=>'zavrsni',
-    'korisnik'=>'edunova',
-    'lozinka'=>'edunova'
-]
+ 'dev'=>$dev,
+ 'url'=>$url,
+ 'naslovApp'=>'Bistro Loora',
+ 'baza'=>$baza
 ];
