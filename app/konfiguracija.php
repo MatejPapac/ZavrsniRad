6cf075ddf0db4cc74d295c8132f18d356a7f7b03
MatejPapac <papac.tmrip@gmail.com>
2022-03-10 12:42:52 +0100

    Update konfiguracija.php

--- app/konfiguracija.php
+++ app/konfiguracija.php
@@ -1,37 +1,28 @@
 <?php
-switch ($_SERVER['SERVER_ADDR']) {
-    // Development or local
-    case '127.0.0.1':
-        $dev = true;
-        $url = 'zavrsni.xyz/';
-        $baza = [
-            'server' => 'localhost',
-            'baza' => 'zavrsni',
-            'korisnik' => 'edunova',
-            'lozinka' => 'edunova'
-        ];
-        break;
-    // Production
-    case '164.92.76.139':
-       
-        $dev = false;
-        $url = 'http://www.zr-papac.shop/';
-        $baza = [
-            'server' => 'localhost',
-            'baza' => 'zavrsni',
-            'korisnik' => 'edunova',
-            'lozinka' => 'edunova'
-        ];
-        break;
 
-    default:
-        echo 'Not valid server adress.';
-        break;
+if($_SERVER['SERVER_ADDR']==='127.0.0.1'){
+    $url='http://zavrsni.xyz/';
+    $dev=true;
+    $baza=[
+        'server'=>'localhost',
+        'baza'=>'zavrsni',
+        'korisnik'=>'edunova',
+        'lozinka'=>'edunova'
+    ];
+}else{
+    $url='http://www.zr-papac.shop/';
+    $dev=false;
+    $baza=[
+        'server'=>'localhost',
+        'baza'=>'zavrsni',
+        'korisnik'=>'root',
+        'lozinka'=>""
+    ];
 }
 
 return [
-    'dev' => $dev,
-    'url' => $url,
-    'naslovApp' => 'Bistro Loora',
-    'baza' => $baza
+ 'dev'=>$dev,
+ 'url'=>$url,
+ 'naslovApp'=>'Bistro Loora',
+ 'baza'=>$baza
 ];
