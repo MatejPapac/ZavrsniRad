76fed3c5d2c362d3b51b327a61a47289a56b3929
MatejPapac <papac.tmrip@gmail.com>
2022-03-24 14:41:23 +0100

    dodavanja kontrola

--- app/controller/PiceController.php
+++ app/controller/PiceController.php
@@ -7,11 +7,30 @@ class PiceController extends AutorizacijaController
      'pice' .DIRECTORY_SEPARATOR;
 
      private $poruka;
+     private $nf;
+     private $pica;
+
+     public function __construct()
+     {
+         parent::__construct();
+         $this->nf = new \NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
+         $this->nf->setPattern('#,##0.00 kn');
+         $this->pica = new stdClass();
+         $this->pica->naziv='';
+         $this->pica->vrsta='1';
+         $this->pica->cijena='';
+   
+     }
+ 
 
 
      public function index ()
      {
-         //print_r(Pice::read());
+$pica = Pice::read();
+        foreach($pica as $pice){
+            $pice->cijena=$this->nf->format($pice->cijena);
+        }
+       
          $this->view->render($this->viewDir . 'index',[
              'pice'=>Pice::read(),
              'css'=>'    <link rel="stylesheet" href="'.App::config('url').'public/css/piceindex.css">'
@@ -25,33 +44,89 @@ class PiceController extends AutorizacijaController
       
    }
 
-   public function promjena($sifra)
+   public function promjena($id)
    {
-       $this->smjer =Pice::readOne($sifra);
-       $this->view->render($this->viewDir. 'novi');
+       $this->pica =Pice::readOne($id);
 
-   }
+
+       if($this->pica->cijena==0){
+           $this->pica->cijena='';
+       }else{
+           $this->pica->cijena=$this->nf->format($this->pica->cijena);
+       }
+
+       $this->view->render($this->viewDir . 'promjena',[
+           'poruka'=>'promjenite podatke',
+           'pica'=>$this->pica
+       ]);
+       }
+     
+
+   
 
 
 
    public function dodajNovi()
    {
-       Pice::create($_POST);
-       $this->index();
+
+    if($this->kontrolaNaziv()
+    && $this->kontrolaCijena()
+    && $this->kontrolaVrsta()) {
+        Pice::create((array)$this->pica);
+        header('location:' . App::config('url') .'pice/index');
+    }else{
+        $this->view->render($this->viewDir.'novi',[
+            'poruka'=>$this->poruka,
+            'pica'=>$this->smjer
+        ]);
+    
+    }
+  
 
    }
     
      public function brisanje($sifra)
     {
         Pice::delete($sifra);
-        $this->index();
+      
+        header('location' .App::config('url') . 'pice/index');
     }
     public function kontrolaVrsta()
     {
         if(strlen($this->pice->vrsta)===0){
             $this->poruka='vrsta obavezno' ;
+            return false;
         }
+        if(strlen($this->pice->vrsta)=false){
+        $this->poruka='vrsta ne postoji';
+        return false;
     }
+return true;
+}
+
+
+
+    private function kontrolaCijena()
+    {
+        if(strlen(trim($this->smjer->cijena))>0){
+
+          
+           
+            $this->pica->cijena = str_replace('.','',$this->pica->cijena);
+          
+            $this->pica->cijena = (float)str_replace(',','.',$this->pica->cijena);
+        
+            if($this->pica->cijena<=0){
+                $this->poruka='Ako unosite cijenu, mora biti decimalni broj veći od 0, unijeli ste: ' 
+            . $this->pica->cijena;
+            $this->pica->cijena='';
+            return false;
+            }
+        }
+
+        return true;
+    }
+
 
     public function kontrolaNaziv()
     {
