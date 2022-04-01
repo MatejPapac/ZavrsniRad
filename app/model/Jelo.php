e9cea8effa2f61f9454976260d6aa8b685337849
MatejPapac <papac.tmrip@gmail.com>
2022-04-01 23:18:48 +0200

    Update Jelo.php

--- app/model/Jelo.php
+++ app/model/Jelo.php
@@ -1,16 +1,96 @@
 <?php
 
+
 class Jelo
 {
+
     public static function readOne($kljuc)
     {
-        $veza=DB::getInstanca();
-        $izraz = $veza ->prepare(
-            '
-            select * from jelo where sifra=:parametar
-            '
-        );
-        $izraz->execute
+  
+      $veza=DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+      select * from jelo where sifra=:parametar;
+    
+      
+      
+      ');
+  
+     $izraz->execute(['parametar'=>$kljuc]);
+     return $izraz->fetch();
     }
+  //CRUD
 
+
+  //C-create
+
+  public static function create($parametri)
+  {
+      $veza = DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+          insert into jelo (naziv,cijena,vrsta)
+          values (:naziv,:cijena,:vrsta);
+      
+      '); 
+    
+      $izraz->execute($parametri);
+      
+  }
+  //R-Read
+
+  public static function read()
+  {
+
+    $veza=DB::getInstanca();
+    $izraz = $veza->prepare('
+    
+    select * from pice
+  
+    
+    
+    ');
+
+   $izraz->execute();
+   return $izraz->fetchAll();
+  }
+
+  //U-update
+
+  
+  public static function update($parametri)
+  {
+      $veza = DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+          update pice set
+            naziv=:naziv,
+            cijena=:cijena,
+            vrsta=:vrsta
+            where sifra =:sifra;
+ 
+      '); 
+    
+      $izraz->execute($parametri);
+  }
+
+  //D-Delete
+
+ 
+  public static function delete($sifra)
+  {
+      $veza = DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+          delete from pice where sifra=:sifra;
+      
+      '); 
+      $izraz->execute(['sifra'=>$sifra]);
+
+  }
 }
+
+//delete  a.sifra,a.cijena,a.vrsta
+//from pice a inner join vrsta b on
+//a.vrsta=b.sifra
+// where a.sifra=1
