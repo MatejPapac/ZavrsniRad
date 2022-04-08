df9cc15a9d543bde616c6f8f46963ec96c187471
MatejPapac <papac.tmrip@gmail.com>
2022-04-08 17:12:17 +0200

    postavljanje funkcija

--- app/controller/JeloController.php
+++ app/controller/JeloController.php
@@ -7,11 +7,56 @@ class JeloController extends AutorizacijaController
      'jelo' .DIRECTORY_SEPARATOR;
 
 
+     private $poruka;
+
+     private $nf;
+
+     private $jela;
+
+
+     public function __construct()
+     {
+         parent::__construct();
+         $this->nf = new \NumberFormatter("hr-HR", \NumberFormatter::DECIMAL);
+         $this->nf->setPattern('#,##0.00 kn');
+         $this ->jela=new stdClass();
+         $this->jela->naziv='';
+         $this->jela->vrsta='1';
+         $this->jela->cijena='';
+
+
+     }
+
+
      public function index ()
      {
          $this->view->render($this->viewDir . 'index',[
-             'jela'=>Jelo::read()
+             'jela'=>Jelo::read(),
+             'css'=>'<link rel="stylesheet" href="'.App::config('url').'public/css/piceindex.css">'
+            
          ]);
+     }
+
+     public function novi()
+     {
+         $this->view->render($this->viewDir . 'novi',[
+             'poruka'=>'',
+             'jela'=>$this->jela
+         ]);
+     }
+
+     public function promjena($id)
+     {
+         $this->jela=Jelo::readOne($id);
+         $this->view->render($this->viewDir . 'promjena',[
+             'poruka'=>'',
+             'jela'=>$this->jela
+         ]);
+     }
+
+     public function promjeni()
+     {
+         
      }
 
      public function brisanje($sifra)
