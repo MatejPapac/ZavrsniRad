db98f99251f34b9f7a1fbaf116c15b42c4e1caa3
MatejPapac <papac.tmrip@gmail.com>
2022-04-11 14:01:54 +0200

    Update RezervacijaController.php

--- app/controller/RezervacijaController.php
+++ app/controller/RezervacijaController.php
@@ -9,21 +9,6 @@ class PiceController extends AutorizacijaController
      private $poruka;
      
      private $rez;
-
-     public function __construct()
-     {
-         parent::__construct();
-    
-    
-         $this->rez = new stdClass();
-         $this->rez->datum='';
-         $this->rez->osoba='1';
-         $this->rez->brojRezerviranihMjesta='';
-         $this->rez->mjesto='';
-         $this->rez->kontakt='';
-         $this->rez->napomena='';
-   
-     }
  
 
 
@@ -37,64 +22,7 @@ class PiceController extends AutorizacijaController
              'css'=>'    <link rel="stylesheet" href="'.App::config('url').'public/css/piceindex.css">'
          ]);
      }
-   public function novi()
-
-   {
-       $this->view->render($this->viewDir. 'novi',[
-           'poruka'=>'Popunite podatke',
-           'rezervacija'=>$this->rezervacija
-       ]);
-         
-      
-   }
-
-   public function promjena($id)
-   {
-       $this->pica =Pice::readOne($id);
-       $this->view->render($this->viewDir . 'promjena',[
-        'poruka'=>'Promijenite podatke',
-        'rezervacija'=> $this->pica
-       ]);
-
-   }
-     
-
-       public function promjeni()
-       {
-    
-    
-         
-        
-      
-         }
-
-    private function pripremiPodatke()
-    {
-        $this->pica=(object)$_POST;
-
-    }
-
-
-
-   public function dodajNovi()
-   {
-    
-
-  
-
-   }
-    
-     public function brisanje($sifra)
-    {
-        Rezervacija::delete($sifra);
-      
-        header('location:' . App::config('url') . 'rezervacija/index');
-
-
-
-        
-       
-    }
+ 
    
 
 
