353f35cf7bd8e7745e6df5141d4239bd8394dc81
MatejPapac <papac.tmrip@gmail.com>
2022-04-11 13:56:38 +0200

    rezervacija

---
+++ app/controller/RezervacijaController.php
@@ -0,0 +1,114 @@
+<?php
+
+class PiceController extends AutorizacijaController
+{
+    private $viewDir=
+    'privatno' . DIRECTORY_SEPARATOR .
+     'rezervacija' .DIRECTORY_SEPARATOR;
+
+     private $poruka;
+     
+     private $rez;
+
+     public function __construct()
+     {
+         parent::__construct();
+    
+    
+         $this->rez = new stdClass();
+         $this->rez->datum='';
+         $this->rez->osoba='1';
+         $this->rez->brojRezerviranihMjesta='';
+         $this->rez->mjesto='';
+         $this->rez->kontakt='';
+         $this->rez->napomena='';
+   
+     }
+ 
+
+
+     public function index ()
+     {
+     $pica = Rezervacija::read();
+      
+       
+         $this->view->render($this->viewDir . 'index',[
+             'rezervacija'=>Rezervacija::read(),
+             'css'=>'    <link rel="stylesheet" href="'.App::config('url').'public/css/piceindex.css">'
+         ]);
+     }
+   public function novi()
+
+   {
+       $this->view->render($this->viewDir. 'novi',[
+           'poruka'=>'Popunite podatke',
+           'rezervacija'=>$this->rezervacija
+       ]);
+         
+      
+   }
+
+   public function promjena($id)
+   {
+       $this->pica =Pice::readOne($id);
+       $this->view->render($this->viewDir . 'promjena',[
+        'poruka'=>'Promijenite podatke',
+        'rezervacija'=> $this->pica
+       ]);
+
+   }
+     
+
+       public function promjeni()
+       {
+    
+    
+         
+        
+      
+         }
+
+    private function pripremiPodatke()
+    {
+        $this->pica=(object)$_POST;
+
+    }
+
+
+
+   public function dodajNovi()
+   {
+    
+
+  
+
+   }
+    
+     public function brisanje($sifra)
+    {
+        Rezervacija::delete($sifra);
+      
+        header('location:' . App::config('url') . 'rezervacija/index');
+
+
+
+        
+       
+    }
+   
+
+
+
+  
+  
+
+
+ 
+
+        
+      
+
+
+  
+        
+    }
---
+++ app/model/Rezervacija.php
@@ -0,0 +1,97 @@
+<?php
+
+class Rezervacija
+{
+
+    public static function readOne($kljuc)
+    {
+  
+      $veza=DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+      select * from rezervacija where sifra=:parametar;
+    
+      
+      
+      ');
+  
+     $izraz->execute(['parametar'=>$kljuc]);
+     return $izraz->fetch();
+    }
+  //CRUD
+
+
+  //C-create
+
+  public static function create($parametri)
+  {
+      $veza = DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+          insert into rezervacija (datum,osoba,brojRezerviranihMjesta,mjesto,kontakt,napomena)
+          values (:datum,:osoba,:brojRezerviranihMjesta,:mjesto,:kontakt,:napomena;
+      
+      '); 
+    
+      $izraz->execute($parametri);
+      
+  }
+  //R-Read
+
+  public static function read()
+  {
+
+    $veza=DB::getInstanca();
+    $izraz = $veza->prepare('
+    
+    select * from rezervacija
+  
+    
+    
+    ');
+
+   $izraz->execute();
+   return $izraz->fetchAll();
+  }
+
+  //U-update
+
+  
+  public static function update($parametri)
+  {
+      $veza = DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+          update rezervacija set
+            datum=:datum,
+            brojRezerviranihMjesta =:brojRezerviranihMjesta,
+            mjesto=:mjesto,
+            kontakt=:kontakt
+            napomena=:napomena
+            where sifra =:sifra;
+ 
+      '); 
+    
+      $izraz->execute($parametri);
+  }
+
+  //D-Delete
+
+ 
+  public static function delete($sifra)
+  {
+      $veza = DB::getInstanca();
+      $izraz = $veza->prepare('
+      
+          delete from rezervacija where sifra=:sifra;
+      
+      '); 
+      $izraz->execute(['sifra'=>$sifra]);
+
+  }
+}
+
+//delete  a.sifra,a.cijena,a.vrsta
+//from pice a inner join vrsta b on
+//a.vrsta=b.sifra
+// where a.sifra=1
