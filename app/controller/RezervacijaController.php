76d580ec0bc90d96a502038c5ad37a218115d0a3
MatejPapac <papac.tmrip@gmail.com>
2022-04-11 14:13:53 +0200

    Update RezervacijaController.php

--- app/controller/RezervacijaController.php
+++ app/controller/RezervacijaController.php
@@ -6,14 +6,30 @@ class RezervacijaController extends AutorizacijaController
     'privatno' . DIRECTORY_SEPARATOR .
      'rezervacija' .DIRECTORY_SEPARATOR;
 
-
+     private $poruka;
      
+     private $rez;
 
+     public function __construct()
+     {
+         parent::__construct();
+    
+    
+         $this->rez = new stdClass();
+         $this->rez->datum='';
+         $this->rez->osoba='1';
+         $this->rez->brojRezerviranihMjesta='';
+         $this->rez->mjesto='';
+         $this->rez->kontakt='';
+         $this->rez->napomena='';
+   
+     }
+ 
 
 
      public function index ()
      {
-    
+     $pica = Rezervacija::read();
       
        
          $this->view->render($this->viewDir . 'index',[
@@ -21,7 +37,55 @@ class RezervacijaController extends AutorizacijaController
              'css'=>'    <link rel="stylesheet" href="'.App::config('url').'public/css/piceindex.css">'
          ]);
      }
- 
+   public function novi()
+
+   {
+       $this->view->render($this->viewDir. 'novi',[
+           'poruka'=>'Popunite podatke',
+           'rezervacija'=>$this->rezervacija
+       ]);
+         
+      
+   }
+
+
+
+       public function promjeni()
+       {
+    
+    
+         
+        
+      
+         }
+
+    private function pripremiPodatke()
+    {
+        $this->pica=(object)$_POST;
+
+    }
+
+
+
+   public function dodajNovi()
+   {
+    
+
+  
+
+   }
+    
+     public function brisanje($sifra)
+    {
+        Rezervacija::delete($sifra);
+      
+        header('location:' . App::config('url') . 'rezervacija/index');
+
+
+
+        
+       
+    }
    
 
 
@@ -30,7 +94,6 @@ class RezervacijaController extends AutorizacijaController
   
 
 
- 
 
         
       
